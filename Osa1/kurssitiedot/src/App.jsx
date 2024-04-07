3ea79c14d10c493a81cb55dcbe7fcc9f616ad959
Laalo Ceesay <>
2024-04-07 16:29:23 +0300

    commit

--- Osa1/kurssitiedot/src/App.jsx
+++ Osa1/kurssitiedot/src/App.jsx
@@ -1,31 +1,36 @@
-import React from 'react'
-
-const Part = (props) =>  {
+const Header = (props) => {
+  // console.log(props)
   return (
     <div>
-    <p>{props.part} {props.exercises}</p>
+      <h1>{props.course}</h1>
     </div>
   )
 }
 
-const Header = (props) => {
-  console.log(props)
-  return <h1>{props.course}</h1>
+const Part = (props) => {
+  // console.log(props)
+  return (
+    <div>
+      <p>
+        {props.part} {props.exercise}
+      </p>
+    </div>
+  )
 }
 
 const Content = (props) => {
-  console.log(props)
+  // console.log(props)
   return (
     <div>
-      <Part part = {props.parts[0].name} exercises = {props.parts[0].exercises} />
-      <Part part = {props.parts[1].name} exercises = {props.parts[1].exercises} />
-      <Part part = {props.parts[2].name} exercises = {props.parts[2].exercises} />
+      <Part part={props.parts[0].name} exercise={props.parts[0].exercises} />
+      <Part part={props.parts[1].name} exercise={props.parts[1].exercises} />
+      <Part part={props.parts[2].name} exercise={props.parts[2].exercises} />
     </div>
   )
 }
 
 const Total = (props) => {
-  console.log(props)
+  // console.log(props)
   return (
     <div>
       <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
@@ -49,16 +54,16 @@ const App = () => {
         name: 'State of a component',
         exercises: 14
       }
-    ] 
+    ]
   }
 
   return (
     <div>
-      <Header course = {course.name} />
-      <Content parts = {course.parts} />
-      <Total parts = {course.parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
-export default App
 
+export default App
